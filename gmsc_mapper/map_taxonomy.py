7a6064c52b75d1eab4afe368fd4c4b9165c3ed20
cocodyq <1057696203@qq.com>
2022-09-26 23:55:01 +0800

    BUG fix default file&no tax mapping&update document

--- gmsc_mapper/map_taxonomy.py
+++ gmsc_mapper/map_taxonomy.py
@@ -17,11 +17,10 @@ def smorf_taxonomy(taxfile,resultfile,tmpdirname):
     print('Finish taxonomy mapping.')
     return taxonomy_file
     
-def add_taxa_to_sseqid(taxa,linelist,original_tax,sseqid):
+def add_taxa_to_sseqid(taxa,og_tax,sseqid):
     taxa[sseqid] = []
-    if len(linelist) == 3:
-        for tax in original_tax.split(";"):
-            taxa[sseqid].append(tax)
+    for tax in og_tax.split(";"):
+        taxa[sseqid].append(tax)
     return(taxa)
 
 def compare_cluster(cluster,taxa,change,lastrank,out):
@@ -60,14 +59,14 @@ def compare_cluster(cluster,taxa,change,lastrank,out):
         else: #for sequence which doesn't have taxonomy
             out.write(f'{q_seqid}\n')     
 
-def deep_lca(taxonomyfile,outdir,resultfile,tmpdirname):
-    taxonomy_file = smorf_taxonomy(taxonomyfile,resultfile,tmpdirname)
-    taxonomy_dlca_file = path.join(outdir,"taxonomy.out.smorfs.tsv")
-	
+def deep_lca(taxfile,outdirname,resultfile,tmpdirname):
+    taxonomy_file = smorf_taxonomy(taxfile,resultfile,tmpdirname)
+    taxonomy_dlca_file = path.join(outdirname,"taxonomy.out.smorfs.tsv")
     cluster = {}
     taxa = {}
     change = {}
     lastrank = ""
+    og_tax = ""
 
     with open(taxonomy_dlca_file, "wt") as out:
         out.write(f'q_seqid\ttaxonomy\n')
@@ -76,8 +75,6 @@ def deep_lca(taxonomyfile,outdir,resultfile,tmpdirname):
                 linelist = line.strip().split("\t")
                 qseqid = linelist[0]
                 sseqid = linelist[1] 	
-                if len(linelist) == 3:
-                    original_tax = linelist[2]
 
                 if cluster:
                     if qseqid in cluster.keys():
@@ -90,10 +87,14 @@ def deep_lca(taxonomyfile,outdir,resultfile,tmpdirname):
                         cluster[qseqid] = [sseqid]
                 else:
                     cluster[qseqid] = [sseqid]
-                
-                taxa = add_taxa_to_sseqid(taxa,linelist,original_tax,sseqid)
-            compare_cluster(cluster,taxa,change,lastrank,out)                                            
 
+                if len(linelist) == 3:
+                    og_tax = linelist[2]                
+                    taxa = add_taxa_to_sseqid(taxa,og_tax,sseqid)
+                else:
+                    taxa[sseqid] = []
+            compare_cluster(cluster,taxa,change,lastrank,out)
+   
 def taxa_summary(outdir):
     taxonomy_dlca_file = path.join(outdir,"taxonomy.out.smorfs.tsv")
     output = pd.read_csv(taxonomy_dlca_file, sep='\t')
@@ -111,42 +112,3 @@ def taxa_summary(outdir):
             rank_percentage[rank[i]] = 0
     annotated_number = output['taxonomy'].size - rank_number['no rank']
     return annotated_number,rank_number,rank_percentage			
-
-'''
-# pandas
-def smorf_taxonomy(args,resultfile,tmpdirname):
-    taxonomy_file = path.join(tmpdirname,"taxonomy.out.smorfs.tmp.tsv")	
-
-    result = pd.read_csv(resultfile, sep='\t',header=None)
-    result = result.rename(columns={0:'qseqid',1:'sseqid'})
-    ref_taxonomy = pd.read_csv(args.taxonomy, sep='\t',header=None)
-    ref_taxonomy.columns = ['sseqid','taxonomy']
-
-    output = pd.merge(result,ref_taxonomy,how='left')[['qseqid', 'sseqid','taxonomy']]
-    output.to_csv(taxonomy_file,sep='\t',index=False)
-    return taxonomy_file
-    
-#sqlite3
-def smorf_taxonomy(taxonomyfile,resultfile,tmpdirname):
-    import sqlite3
-    print('Start taxonomy mapping...')
-    taxonomy_file = path.join(tmpdirname,"taxonomy.out.smorfs.tmp.tsv")	
-    conn = sqlite3.connect(taxonomyfile)
-    tax = sqlite3.connect(':memory:')
-    conn.backup(tax)
-    conn.close()
-    cursorObj = tax.cursor()
-    
-    with open(taxonomy_file,"wt") as out:
-        with open(resultfile,"rt") as f:
-            for line in f:
-                linelist = line.strip().split("\t")
-                sseqid = int(linelist[1].split('.')[2].replace("_",""))
-                sql = f'SELECT * FROM taxonomy WHERE smorf = "{sseqid}"'
-                cursorObj.execute(sql)
-                searched = cursorObj.fetchone()
-                out.write(f'{linelist[0]}\t{str(searched[0])}\t{searched[1]}\n')
-    tax.close()
-    print('Finish taxonomy mapping.')
-    return taxonomy_file
-'''
