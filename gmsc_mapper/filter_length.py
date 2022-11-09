c32b38fb02d8b6407460bfa288ef6e939652790f
Luis Pedro Coelho <luis@luispedro.org>
2022-11-09 08:26:53 +0800

    MIN Fix whitespace in warning message
    Also, use booleans instead of 0/1

--- gmsc_mapper/filter_length.py
+++ gmsc_mapper/filter_length.py
@@ -1,24 +1,21 @@
-message_error = '''GMSC-mapper Error: Input sequences are all more than 303nt or 100aa,which will be filtered. 
-                   Please check if your input consist of contigs, which should use -i not --nt-genes or --aa-genes as input.\n'''
+message_error = '''GMSC-mapper Error: Input sequences are all more than 303nt or 100aa,which will be filtered.
+Please check if your input consist of contigs, which should use -i not --nt-genes or --aa-genes as input.\n'''
 
 def filter_length(queryfile, tmpdirname, N):
     import sys
     import gzip
     from os import path
     from .fasta import fasta_iter
-    
     filtered_file = path.join(tmpdirname, "filtered.faa.gz")
 
     with gzip.open(filtered_file, 'wt',compresslevel=1) as of:
-        all_longer_flag = 1
-        
+        all_longer_flag = True
         for ID, seq in fasta_iter(queryfile):
             if len(seq) < N:
-                all_longer_flag = 0
+                all_longer_flag = False
                 of.write(f'>{ID}\n{seq}\n')
-        
+
         if all_longer_flag:
             sys.stderr.write(message_error)
             sys.exit(1)
-    
     return filtered_file
