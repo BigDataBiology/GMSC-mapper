d035a5f9824145d0e752e7e974b59b00dd014435
Luis Pedro Coelho <luis@luispedro.org>
2024-01-11 15:21:39 +1000

    RLS Version 0.1.0
    First release

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-from os import path
-from setuptools import setup, find_packages
+from setuptools import setup
+
+exec(compile(open('gmsc_mapper/gmsc_mapper_version.py').read(),
+             'gmsc_mapper/gmsc_mapper_version.py', 'exec'))
+
+long_description = open('README.md', encoding='utf-8').read()
 
 setup(
-    name="GMSC-mapper",  
-    version="0.0.1a0", 
-    description="Command line tool to query the Global Microbial smORFs Catalog (GMSC)",  
-    long_description=open("./README.md", "r").read(),  
-    long_description_content_type="text/markdown",  
-    url="https://github.com/BigDataBiology/GMSC-mapper",  
+    name="GMSC-mapper",
+    version=__version__,
+    description="Command line tool to query the Global Microbial smORFs Catalog (GMSC)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BigDataBiology/GMSC-mapper",
     author="Yiqian Duan",
-    author_email="yqduan20@fudan.edu.cn",  
+    author_email="yqduan20@fudan.edu.cn",
     license="MIT",
-    classifiers=[  
-        "Development Status :: 3 - Alpha",
+    classifiers=[
+        "Development Status :: 4 - Beta",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: MIT License",
         'Programming Language :: Python :: 3.7',
@@ -21,10 +25,10 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
     ],
-    packages=['gmsc_mapper'],  
+    packages=['gmsc_mapper'],
     install_requires=open('./requirements.txt', 'r').read().splitlines(),
     zip_safe=False,
-    entry_points={  
+    entry_points={
         "console_scripts": ['gmsc-mapper=gmsc_mapper.main:main'],
     }
 )
